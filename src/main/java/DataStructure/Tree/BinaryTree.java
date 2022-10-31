209c9c83967d8354fadd0607f3a462b36833f2aa
KeXin Chen <1139822604@qq.com>
2022-10-31 11:10:23 +0800

    Add some code about Tree

--- src/main/java/DataStructure/Tree/BinaryTree.java
+++ src/main/java/DataStructure/Tree/BinaryTree.java
@@ -31,4 +31,55 @@ public class BinaryTree {
             System.out.println("空二叉树，无法遍历");
         }
     }
+    /**
+     * 根据节点编号的前序查询
+     */
+
+    public Node preNode(int no){
+        if(root!=null){
+            return root.preSearch(no);
+        }else {
+            System.out.println("空二叉树，无法遍历");
+            return null;
+        }
+    }
+
+    /**
+     * 根据节点编号的中序查询
+     */
+    public Node infixNode(int no){
+        if(root!=null){
+            return root.infixSearch(no);
+        }else {
+            System.out.println("空二叉树，无法遍历");
+            return null;
+        }
+    }
+
+    /**
+     * 根据节点编号的后序查询
+     */
+    public Node postNode(int no){
+        if(root!=null){
+            return root.postSearch(no);
+        }else {
+            System.out.println("空二叉树，无法遍历");
+            return null;
+        }
+    }
+
+    /**
+     * 删除节点
+     */
+    public void delNode(int no){
+        if(root!=null){
+            if(root.getNo()==no){
+                root = null;
+            }else {
+                root.delNode(no);
+            }
+        }else {
+            System.out.println("空二叉树，无法删除");
+        }
+    }
 }
--- src/main/java/DataStructure/Tree/Node.java
+++ src/main/java/DataStructure/Tree/Node.java
@@ -9,9 +9,9 @@ public class Node {
     /**
      *
      */
-    public Node(int no,String name){
-        this.no=no;
-        this.name=name;
+    public Node(int no, String name) {
+        this.no = no;
+        this.name = name;
     }
 
     public int getNo() {
@@ -59,13 +59,13 @@ public class Node {
     /**
      * 定义前序遍历
      */
-    public void preSelect(){
+    public void preSelect() {
         //先输出父节点
         System.out.println(this);
-        if(this.left !=null){
+        if (this.left != null) {
             this.left.preSelect();
         }
-        if(this.right !=null){
+        if (this.right != null) {
             this.right.preSelect();
         }
     }
@@ -74,13 +74,13 @@ public class Node {
     /**
      * 中序遍历
      */
-    public void infixSelect(){
+    public void infixSelect() {
         //左节点
-        if(this.left!=null){
+        if (this.left != null) {
             this.left.infixSelect();
         }
         System.out.println(this);
-        if(this.right!=null){
+        if (this.right != null) {
             this.right.infixSelect();
         }
     }
@@ -89,14 +89,130 @@ public class Node {
     /**
      * 后序遍历
      */
-    public void postSelect(){
-        if(this.left!=null){
+    public void postSelect() {
+        if (this.left != null) {
             this.left.postSelect();
         }
-        if(this.right!=null){
+        if (this.right != null) {
             this.right.postSelect();
         }
         System.out.println(this);
+
+    }
+
+    /**
+     * 前序遍历查找
+     */
+    public Node preSearch(int no) {
+        //看看是否是当前节点
+        if (this.no == no) {
+            return this;
+        }
+        //查询左子节点
+        Node lNode = null;
+        if (this.left != null) {
+            lNode = this.left.preSearch(no);
+        }
+        if (lNode != null) {
+            return lNode;
+        }
+
+        //查询当前节点的右子节点，如果不为空，则继续递归前序查找
+        if (this.right != null) {
+            lNode = this.right.preSearch(no);
+        }
+        return lNode;
+
+    }
+
+    /**
+     * 中序遍历查找
+     */
+    public Node infixSearch(int no) {
+        Node node = null;
+        if (this.left != null) {
+            node = this.left.infixSearch(no);
+        }
+        if (node != null) {
+            return node;
+        }
+
+        //对比当前节点
+        if (this.no == no) {
+            return this;
+        }
+        //遍历右子节点
+        if (this.right != null) {
+            node = this.right.infixSearch(no);
+        }
+        return node;
+
+
+    }
+
+    /**
+     * 后序遍历查找
+     */
+    public Node postSearch(int no){
+        Node node = null;
+        if (this.left != null) {
+            node = this.left.postSearch(no);
+        }
+
+        if (node != null) {
+            return node;
+        }
+
+        if (this.right != null) {
+            node = this.right.postSearch(no);
+        }
+        if(node!=null){
+            return node;
+        }
+        //对比当前节点
+        if (this.no == no) {
+            return this;
+        }
+
+        return node;
+    }
+
+    /**
+     * 删除节点两种情况
+     * 1.删除的节点是叶子节点
+     * 2.删除的是子树，非叶子节点
+     */
+    public void delNode(int no){
+        /**
+         * 1.如果当前节点左节点不为空，且左节点就是要删除的节点，this。left=null
+         * 2.如果当前节点右节点不为空，且右节点就是要删除的节点，this。right=right
+         * 3.如果第一步和第二步没有执行，那么需要向左子树进行递归删除
+         * 4.如果第三步没有执行，则向右子树进行递归删除。
+         */
+        if(this.left!=null&&this.left.no ==no){
+            this.left = null;
+            return;
+        }
+        if(this.right!=null&&this.right.no ==no){
+            this.right = null;
+            return;
+        }
+
+        /**
+         * 向左子树进行递归删除
+         */
+        if(this.left!=null){
+            this.left.delNode(no);
+        }
+
+
+        /**
+         * 向右子树进行递归删除
+         */
+        if(this.right!=null){
+            this.right.delNode(no);
+        }
+
 
     }
 }
