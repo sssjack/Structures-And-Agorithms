287f37b1188ddc0897ea8a83d18c1cabb9eb5391
Chenkexin <chenkx@tianyuaninfo.com>
2022-11-10 19:25:12 +0800

    Add some code about HuffmanTree

--- src/main/java/DataStructure/Tree/HuffmanTree/HuffmanTree.java
+++ src/main/java/DataStructure/Tree/HuffmanTree/HuffmanTree.java
@@ -1,30 +1,72 @@
 package DataStructure.Tree.HuffmanTree;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class HuffmanTree {
     public static void main(String[] args) {
         int[] array = {13,7,8,29,6,1};
 
+        Node root = createHuffmanTree(array);
+
+        preSelect(root);
+
+
 
     }
-
     /**
-     * 把数列转赫夫曼树
+     * 创建赫夫曼树方法
+     * @return
      */
     public static Node createHuffmanTree(int[] array){
         /**
          * 1.遍历数组
-         * 2.将每一个元素构成一个节点Node
-         * 3.将Node放入到集合中，并且是排好的，从小到大
+         * 2.讲每一个元素构建成一个结点Node
+         * 3.讲Node放入一个集合中
          */
         List<Node> nodes = new ArrayList<Node>();
-        for(int i:array){
-//            将元素转为Node对象
+        for (int i : array) {
             nodes.add(new Node(i));
         }
-        return null;
+
+        /**
+         * 循环处理结点
+         */
+        while (nodes.size()>1){
+            //从小到大进行排序
+            Collections.sort(nodes);
+
+            System.out.println("集合中数据："+nodes);
+
+            //取出来根结点权值最小的两课二叉树
+            Node leftNode = nodes.get(0);
+            Node rightNode = nodes.get(1);
+
+            //构建一课新的二叉树
+            Node root = new Node(leftNode.value+rightNode.value);
+            root.left = leftNode;
+            root.right = rightNode;
+
+
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
+
+            //将root加入到nodes集合中
+            nodes.add(root);
+
+
+        }
+        return nodes.get(0);
+
+    }
+
+    /**
+     * 前序设置
+     */
+    public static void preSelect(Node root){
+        if (root !=null){
+            root.preSelect();
+        }else {
+            System.out.println("当前是空树，无数据...");
+        }
     }
 }
--- src/main/test/test1.java
+++ src/main/test/test1.java
@@ -66,4 +66,5 @@ public class test1 {
     }
 
 
+
 }
